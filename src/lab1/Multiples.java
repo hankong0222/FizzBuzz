5cce86814cc96d85b5cfcdfeaf1a6c6e826accd7
Lotus <hankong.kong@mail.utoronto.ca>
2025-09-08 23:06:51 -0400

    All test passed

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,20 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
         int j = 0;
 
-        for(int i=1;i<=1000;i++){
-            if (i%3==0) {
+        for(int i = 1; i < n; i++){
+            if (i % a==0) {
                 j++;
             }
-            else if (i%5==0) {
+            else if (i % b ==0) {
                 j++;
             }
         }
-        System.out.println(j);
+        return j;
     }
 }
