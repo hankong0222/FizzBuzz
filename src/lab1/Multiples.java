50e8c1df403a1b277b31a2b0f7e5c9a6623bba42
Lotus <hankong.kong@mail.utoronto.ca>
2025-09-08 23:59:22 -0400

    Added an overlapping method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,6 +5,10 @@ public class Multiples {
         System.out.println(multiples(1000, 3, 5));
     }
 
+    public static int multiles(){
+        return multiples(1000, 3, 5);
+    }
+
     public static int multiples(int n, int a, int b) {
         int j = 0;
 
